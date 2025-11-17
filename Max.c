87f008688b007c1adfb2c45a1300fac212a6f9e2
eladmorbk11 <eladmorbk11@gmail.com>
2025-11-17 14:04:34 +0200

    Delete Max.c- consider: max.c ,max.exe

--- Max.c
+++ Max.c
@@ -1,15 +1,1 @@
-/*
-filename: Max.c
-prints the max of x,y
-Coder: Elad
-*/
 
-#include <stdio.h>
-
-int main(){
-    int x = 0;
-    int y= 1;
-    int z = (x > y) ? x : y ;
-    printf("max is %d",z);
-    return 0;
-}
